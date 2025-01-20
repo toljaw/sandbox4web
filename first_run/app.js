b06949a3a547f302ebc3f9dc381e628f758e3af5
Anatoli Weikum <weikum.anatoli@gmail.com>
2025-01-20 12:50:43 +0100

    flexbox exer

---
+++ first_run/app.js
@@ -0,0 +1,23 @@
+/*
+document.querySelector("h1").addEventListener('click', function() {
+    alert("H1");
+});
+document.querySelector("h2").addEventListener('click', function() {
+    alert("H2");
+});
+document.querySelector("h3").addEventListener('click', function() {
+    document.querySelector("h3").style.backgroundColor = "orange"
+    alert("H3");
+});
+*/
+/*
+const h1 = document.querySelector("h1")
+const h2 = document.querySelector("h2")
+const h3 = document.querySelector("h3")
+
+h1.addEventListener('click', function() {
+    h1.style.backgroundColor = 'black';
+    alert('h1h1h1h1h1h1h1');
+})
+*/
+
